1174ed94eb3d63d1e58a3afeb4028b150eb9f46b
Karume-lab <danielkarume@outlook.com>
2024-04-11 21:07:40 +0300

    fix typo

--- crypto_ai_frontend/src/components/Header.jsx
+++ crypto_ai_frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='text-center'>
             <h1 className='font-bold text-3xl'>CryptoGopher</h1>
-            <span>Practical Summaries of Top Crypto Influenders</span>
+            <span>Practical Summaries of Top Crypto Influencers</span>
         </div>
     )
 }
