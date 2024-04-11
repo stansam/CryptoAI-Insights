f440b8c4c5772483a15d4a1ab4d46281f27ce367
Karume-lab <danielkarume@outlook.com>
2024-04-11 20:14:14 +0300

    finish sidebar

--- crypto_ai_frontend/src/components/Sidebar.jsx
+++ crypto_ai_frontend/src/components/Sidebar.jsx
@@ -2,28 +2,38 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <section>
+        <section className='w-1/4 p-3 h-screen bg-yellow-200 pt-10'>
             <div>
-                <h2>Look up Recommendations</h2>
-                <ul>
-                    <li>
+                <h2 className='font-bold my-5 text-xl'>Look up Recommendations</h2>
+                <ul className='font-semibold'>
+                    <li className='hover:-translate-y-1 hover:transition-transform m-4'>
                         <a href="https://localhost:3000/">By Influencer</a>
+                    </li>
+                    <li className='hover:-translate-y-1 hover:transition-transform m-4'>
                         <a href="https://localhost:3000/">By Token</a>
+                    </li>
+                    <li className='hover:-translate-y-1 hover:transition-transform m-4'>
                         <a href="https://localhost:3000/">By Social Media Platform</a>
-                        <div>
-                            <a href="https://localhost:3000/">By Category</a>
-                            <ul>
-                                <li>
-                                    <a href="https://localhost:3000/">Stable Coins</a>
-                                </li>
-                                <li>
-                                    <a href="https://localhost:3000/">Alt Coins</a>
-                                </li>
-                                <li>
-                                    <a href="https://localhost:3000/">Meme Coins</a>
-                                </li>
-                            </ul>
-                        </div>
+                    </li>
+                    <li className='m-4'>
+                        <a href="https://localhost:3000/">By Category</a>
+                        <ul>
+                            <li className='hover:-translate-y-1 hover:transition-transform m-4 font-normal text-gray-600'>
+                                <a href="https://localhost:3000/">Stable Coins</a>
+                            </li>
+                            <li className='hover:-translate-y-1 hover:transition-transform m-4 font-normal text-gray-600'>
+                                <a href="https://localhost:3000/">Alt Coins</a>
+                            </li>
+                            <li className='hover:-translate-y-1 hover:transition-transform m-4 font-normal text-gray-600'>
+                                <a href="https://localhost:3000/">Meme Coins</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className='hover:-translate-y-1 hover:transition-transform m-4'>
+                        <a href="https://localhost:3000/">Porfolios of Influencers</a>
+                    </li>
+                    <li className='hover:-translate-y-1 hover:transition-transform m-4'>
+                        <a href="https://localhost:3000/">Portfolio Competition (Beat the Experts)</a>
                     </li>
                 </ul>
             </div>
